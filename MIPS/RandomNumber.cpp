16c2aa64d44e5e592b87d8cc2f4fb33bf4644fea
Li Zhehao <47491676+Ricahrd-Li@users.noreply.github.com>
2019-04-17 23:42:04 +0800

    Update RandomNumber.cpp

--- MIPS/RandomNumber.cpp
+++ MIPS/RandomNumber.cpp
@@ -1,15 +1,21 @@
 #include<cstdlib>
 #include<cstdio> 
+#include<time.h>
 using namespace std;
 //生成begin--begin+x内的int
 int Rand(int x, int begin = 0) {
 	return begin + (rand() % x);
 }
 int main(){
+    srand((int)time(0));  // important！
     FILE* fp=fopen("random.txt", "w");
-    for(int i=0;i<999;i++){
-        fprintf(fp, "%d", Rand(1000));
-        fwrite("\n",sizeof(char),1,fp);
+    for(int i=0;i<=999;i++){
+    	if(i<999){
+	        fprintf(fp, "%d", Rand(1000));
+	        fwrite(",",sizeof(char),1,fp);
+		}
+		else  fprintf(fp, "%d", Rand(1000));
+        
     }
 }
 
