c7152603f1ed69b191d4c834a117f2b10a0d1dc2
Li Zhehao <47491676+Ricahrd-Li@users.noreply.github.com>
2019-04-24 19:40:42 +0800

    Update ALU.v

--- lab1/ALU.v
+++ lab1/ALU.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 2019/03/20 08:34:22
+// Create Date: 2019/03/29 09:14:00
 // Design Name: 
 // Module Name: ALU
 // Project Name: 
@@ -18,71 +18,53 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
 //This is an ALU
 module ALU(
     input clk, 
     input [2:0]s,
     input [5:0]a,
     input [5:0]b,
-    output reg[1:0]f,
-    output reg[5:0]result
+    output reg cf, //cf: non-signed carry/borrow; of:signed overflow; zf: zero
+    output reg of,
+    output reg zf,
+    output reg[5:0]y //result
 );
-//s: 000--add, 001--sub, 010--and, 011--or, 100--not, 101--xor
-//N-Normal,O-Overflow/Carry,B=Borrow, Z=Zero
-parameter N=2'b00; 
-parameter OC=2'b01;
-parameter B=2'b10;
-parameter Z=2'b11;
-
-reg[6:0]tmpr; //temp_result
+//s: 000--add, 001--sub, 010--and, 011--or, 100--not, 101--xor,
+//   110--leftshift,111--rightshift
 
 always @(posedge clk) begin
     case (s)  
     //add
       3'b000: begin
-        tmpr={1'b0,a}+{1'b0,b};
-        //deal with f
-        if(tmpr[6]==1'b1) f<=OC;
-        else if(tmpr==7'b000_0000) f<=Z;
-        else f<=N;
-        result=tmpr[5:0];
+        {cf,y}={1'b0,a}+{1'b0,b};
+        of=(~a[5]&~b[5]&y[5]|a[5]&b[5]&~y[5]);
       end
     //sub
       3'b001: begin
-        tmpr={1'b1,a};
-        tmpr=tmpr-{1'b0,b};
-        //deal with f
-        if(tmpr[6]==1'b0) f<= B;
-        else if(tmpr==7'b000_0000) f<=Z;
-        else f<=N;
-        result=tmpr[5:0];
+        {cf,y}=a-b;
+        of=(~a[5]&b[5]&~y[5]|a[5]&~b[5]&~y[5]);
       end
     //and  
-       3'b010: begin
-        result=a&b;
-        if(result==6'b000_0000) f<=Z;
-        else f<=N;
-       end
+      3'b010: y=a&b;
     //or 
-       3'b011: begin
-        result=a|b;
-        if(result==6'b000_0000) f<=Z;
-        else f<=N;
-       end
+      3'b011: y=a|b;
     //not 
-       3'b100: begin
-        result=~a;
-        if(result==6'b000_0000) f<=Z;
-        else f<=N;
-       end
+      3'b100: y=~a;
     //xor 
-      default: begin
-        result=a^b;
-        if(result==6'b000_0000) f<=Z;
-        else f<=N;
-       end
+      3'b101: y=a^b;
+    //leftshift
+      3'b110: begin
+        y={a[4:0],1'b0};
+      end 
+    //rightshift
+      default:begin
+        y={1'b0,a[5:1]};
+      end
     endcase
+
+    zf=~|y;
+
 end
 
 endmodule 
+
